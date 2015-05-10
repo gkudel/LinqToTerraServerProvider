4a9af5f1a838caf935c209e88ec2c9112bf6f8f2
Grzegorz Kudlacz <gkudel@gmail.com>
2015-05-10 19:57:48 +0200

    Init

---
+++ ClientApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using LinqToTerraServerProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryableTerraServerData<Place> terraPlaces = new QueryableTerraServerData<Place>();
+
+            string s = "Johannesburg";
+            var query = from place in terraPlaces
+                        where place.Name == s && place.PlaceType == PlaceType.Island
+                        select place.PlaceType;
+
+            foreach (PlaceType placeType in query)
+                Console.WriteLine(placeType);
+        }
+    }
+}
